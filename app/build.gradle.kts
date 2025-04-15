e2109cb27e47f2dc09b8b394da0e79e97632e9b4
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-04-15 12:05:56 +0300

    Rename apk file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
         applicationId = "io.github.vikulin.opengammakit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        setProperty("archivesBaseName", "ogk-inspector-$versionName")
     }
 
     buildTypes {
