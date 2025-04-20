5ea6ecc7681ed3b4c4f4a186570bdf26919b79b3
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-04-20 22:06:03 +0300

    Changed spectrumDataSet type to OpenGammaKit for multiple spectrum graphs on the same chart. Moved elapsed time to chart legend labels and replaced it with remaining time TextView.

--- app/src/main/kotlin/io/github/vikulin/opengammakit/model/OpenGammaKitData.kt
+++ app/src/main/kotlin/io/github/vikulin/opengammakit/model/OpenGammaKitData.kt
@@ -1,34 +1,35 @@
 package io.github.vikulin.opengammakit.model
 
 import kotlinx.serialization.Serializable
+import java.io.Serializable as JavaSerializable
 
 @Serializable
 data class OpenGammaKitData(
     val schemaVersion: String,
-    val data: List<GammaKitEntry>
-)
+    var data: MutableList<GammaKitEntry>
+) : JavaSerializable
 
 @Serializable
 data class GammaKitEntry(
     val deviceData: DeviceData,
     val resultData: ResultData
-)
+) : JavaSerializable
 
 @Serializable
 data class DeviceData(
     val softwareName: String,
     val deviceName: String
-)
+) : JavaSerializable
 
 @Serializable
 data class ResultData(
     val energySpectrum: EnergySpectrum
-)
+) : JavaSerializable
 
 @Serializable
 data class EnergySpectrum(
-    val numberOfChannels: Int,
-    val measurementTime: Long,
-    val spectrum: List<Long>,
-    val validPulseCount: Long,
-)
+    var numberOfChannels: Int,
+    var measurementTime: Long,
+    var spectrum: List<Long>,
+    var validPulseCount: Long,
+) : JavaSerializable
